f98c0db506d80a8ef3edd1feb55934718e891b2c
unsatifsed <unsatifsederror@gmail.com>
2022-07-23 19:17:24 +0530

    sweet: Officially CipherOs BringUp

--- cipher_sweet.mk
+++ cipher_sweet.mk
@@ -12,15 +12,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cipher/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := cipher_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Cipher Ify
+CIPHER_MAINTAINER := UnsatifsedError
+CIPHER_OFFICIAL := true
+CIPHER_GAPPS := true
 
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED := true
